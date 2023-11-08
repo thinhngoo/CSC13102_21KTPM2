32fa259ca0f87626ed31c323f84c0ffb8453c9c3
thinhngoo <ncathinh21@clc.fitus.edu.vn>
2023-11-08 23:33:39 +0700

    :sparkles: Common requirements and Feature 1, 2, 3

--- slang_word/src/app/SlangDictionary.java
+++ slang_word/src/app/SlangDictionary.java
@@ -1,19 +1,99 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class SlangDictionary {
     private HashMap<String, String> slangWords;
+    private LinkedHashSet<String> searchHistory;
 
     public SlangDictionary() {
         slangWords = new HashMap<>();
+        searchHistory = new LinkedHashSet<>();
+    }
+
+    public void loadDefaultFile() {
+        String filePath = "./data/slang.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine(); // Skip header
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("`");
+                if (parts.length >= 2) {
+                    String word = parts[0];
+                    String meaning = parts[1];
+                    slangWords.put(word, meaning);
+                } else {
+                    System.out.println("Ignoring line: " + line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveToFile(String filePath) {
+        try {
+            FileOutputStream fos = new FileOutputStream(filePath);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(slangWords);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public void loadFromFile(String filePath) {
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            slangWords = (HashMap) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return;
+        }
     }
 
     public void addWord(String word, String meaning) {
         slangWords.put(word, meaning);
     }
 
-    public String getMeaning(String word) {
+    // Feature 1
+    public String searchBySlangWord(String word) {
+        searchHistory.add(word);
         return slangWords.get(word);
+    }
+
+    // Feature 2
+    public List<String> searchByDefinition(String keyword) {
+        List<String> matchingWords = new ArrayList<>();
+        for (Map.Entry<String, String> entry : slangWords.entrySet()) {
+            if (entry.getValue().contains(keyword)) {
+                matchingWords.add(entry.getKey());
+            }
+        }
+        return matchingWords;
+    }
+
+    // Feature 3
+    public List<String> getSearchHistory() {
+        return new ArrayList<>(searchHistory);
     }
 }
--- slang_word/src/app/SlangDictionaryUI.java
+++ slang_word/src/app/SlangDictionaryUI.java
@@ -1,13 +1,17 @@
 package app;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label ;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class SlangDictionaryUI extends Application {
@@ -15,31 +19,59 @@ public class SlangDictionaryUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
-        primaryStage.show();
-        
-        TextField textField = new TextField();
-        Button button = new Button("Search");
-        Label label = new Label();
+        Scene scene = new Scene(featureMenu());
+        primaryStage.setTitle("Slang Dictionary");
+        primaryStage.setScene(scene);
+        primaryStage.show(); 
+    }
 
-        button.setOnAction(e -> {
-            String word = textField.getText();
-            String meaning = dictionary.getMeaning(word);
-            if (meaning == null) {
-                label.setText("Word not found");
-            } else {
-                label.setText(meaning);
+    private Region featureMenu() {
+        ListView<String> listView = new ListView<>();
+
+        ObservableList<String> items = FXCollections.observableArrayList (
+            "1 - Tải dữ liệu mặc định (slang.txt)",
+            "2 - Tìm kiếm theo Slang word",
+            "3 - Tìm kiếm theo Definition",
+            "4 - Hiển thị lịch sử tìm kiếm",
+            "5 - Thêm Slang word mới",
+            "6 - Sửa Slang word",
+            "7 - Xóa Slang word",
+            "8 - Phục hồi lại dữ liệu ban đầu",
+            "9 - Hiển thị 1 slang word ngẫu nhiên",
+            "10 - Đố vui (Slang word -> Definition)",
+            "11 - Đố vui (Definition -> Slang word)"
+            );
+
+        listView.setItems(items);
+
+        listView.setOnMouseClicked(event -> {
+            int selectedIndex = listView.getSelectionModel().getSelectedIndex();
+            System.out.println("Selected feature " + (selectedIndex + 1));
+        
+            switch (selectedIndex) {
+                case 0:
+                    dictionary.loadDefaultFile();
+                    break;
+                case 1:
+                    System.out.println(dictionary.searchBySlangWord("C/O"));
+                    break;
+                case 2:
+                    System.out.println(dictionary.searchByDefinition("Brother"));
+                    break;
+                case 3:
+                    System.out.println(dictionary.getSearchHistory());
+                    break;
+                default:
+                    break;
             }
         });
 
-        VBox vbox = new VBox(textField, button, label);
-        Scene scene = new Scene(vbox, 200, 100);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        HBox vbox = new HBox(listView);
+        vbox.setSpacing(6);
+        return vbox;
     }
+
+    
 
     public static void main(String[] args) {
         launch(args);
